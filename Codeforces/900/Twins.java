2dfbc55f96cfc5f1a0c100c01b0f9a4ad86d7d41
Kapil <kapil@natya-note.local>
2026-08-06 00:08:41 +0530

    Codeforces(900)-160A - Twins problem solved

---
+++ Codeforces/900/Twins.java
@@ -0,0 +1,40 @@
+package Codeforces.900;
+
+import java.util.*;
+
+public class Twins {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] coins = new int[n];
+        int totalSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            coins[i] = sc.nextInt();
+            totalSum += coins[i];
+        }
+
+        Arrays.sort(coins);
+
+        int mySum = 0;
+        int count = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            mySum += coins[i];
+            count++;
+
+            if (mySum > totalSum - mySum) {
+                break;
+            }
+        }
+
+        System.out.println(count);
+
+        sc.close();
+    }
+}
